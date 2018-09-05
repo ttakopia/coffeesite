8ca52d1e1b3fb354de6f5190b6f299ad4c21575e
shohei.k <s.kihara0316@gmail.com>
2018-09-05 17:17:38 +0900

    Revert "sorcery-1"
    This reverts commit ed60d615fbb535319084adcd40b72e6770b80f5b.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180904143717) do
+ActiveRecord::Schema.define(version: 20180826042730) do
 
   create_table "arts", force: :cascade do |t|
     t.string "name"
@@ -75,23 +75,6 @@ ActiveRecord::Schema.define(version: 20180904143717) do
     t.string "name"
     t.integer "taggings_count", default: 0
     t.index ["name"], name: "index_tags_on_name", unique: true
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "crypted_password"
-    t.string "salt"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "remember_me_token"
-    t.datetime "remember_me_token_expires_at"
-    t.string "reset_password_token"
-    t.datetime "reset_password_token_expires_at"
-    t.datetime "reset_password_email_sent_at"
-    t.integer "access_count_to_reset_password_page", default: 0
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
 end
