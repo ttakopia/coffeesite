1b29ea1e393da01fc83c675c27a2f4d21be2c510
shohei.k <s.kihara0316@gmail.com>
2018-09-05 21:47:39 +0900

    sorcery-2

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+	authenticates_with_sorcery!
+
+	validates :password, length: {minimum: 6}
+	validates :password, confirmation: true
+ 	validates :password_confirmation, presence: true
+    validates :email, uniqueness: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  resources :arts
+  resources :user_sessions
+  resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
+
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'user_sessions/destroy'
+
+  resources :users
   get 'index',  to: 'arts#index'
   get 'arts/show'
   get 'new', 	to: 'arts#new'
@@ -9,6 +21,5 @@ Rails.application.routes.draw do
 
   get '/help',      to: 'static_pages#help'
 
-  resources :arts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
