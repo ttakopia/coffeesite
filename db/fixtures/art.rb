1fbb089e1cd854c450b28a7c12db3e7ba035c40d
shohei.k <s.kihara0316@gmail.com>
2018-12-29 19:17:16 +0700

    css revised about art show

--- db/fixtures/art.rb
+++ db/fixtures/art.rb
@@ -1,36 +1,36 @@
 
 samples = [
-	Rails.root.join("db/image/1.jpg").open,
-	Rails.root.join("db/image/2.jpg").open,
-	Rails.root.join("db/image/3.jpg").open,
-	Rails.root.join("db/image/4.jpg").open,
-	Rails.root.join("db/image/5.jpg").open,
-	Rails.root.join("db/image/6.jpg").open,
-	Rails.root.join("db/image/7.jpg").open
+	Rails.root.join("db/image/1-1.jpg").open,
+	Rails.root.join("db/image/1-2.jpg").open,
+	Rails.root.join("db/image/1-3.jpg").open,
+	Rails.root.join("db/image/1-4.jpg").open,
+	Rails.root.join("db/image/1-5.jpg").open,
+	Rails.root.join("db/image/1-6.jpg").open,
+	Rails.root.join("db/image/1-7.jpg").open
 ]
 
 Art.seed do |s|
 	s.id				= 1
-	s.storename 		= "First Cafe"
-	s.address			= "大阪府吹田市津雲台1-1-4"
-	s.tel				= "00-0000-0000"
-	s.web				= "URL"
-	s.time				= "09:00〜17:00"	
-	s.day				= "日曜日"
-	s.area				= "大阪"
-	s.how				= "ドリップ"
-	s.tag_list			= "大阪"
-	s.coffeebean_list 	= "ブルーマウンテン"
-	s.content			= "この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を"
-	s.description 		= "大阪駅から徒歩5分"
-	s.inttitle1			= "タイトルのサンプル"
-	s.intcontent1		= "この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ"	
-	s.inttitle2			= "タイトルのサンプル"
-	s.intcontent2		= "サンプル"
-	s.intprofname		= "サンプルネーム"
-	s.intprofcontent	= "サンプルコンテンツ"
-	s.intprofmenu		= "サンプルメニュー"
-	s.imagesprof		= "#{Rails.root}/db/image/1.jpg"
+	s.storename 		= "Kota Tua Kopi"
+	s.address			= "Jl. Saleh Sungkar No.16F, Kawasan Kota Tua, Bintaro, Ampenan, Kota Mataram"
+	s.tel				= "0878-6567-2974"
+	s.web				= "kotatua-kopi.business.site"
+	s.time				= "10:00~24:00"	
+	s.day				= "Mon-Sun"
+	s.area				= "Lombok(Indonesia)"
+	s.how				= "Filter Coffee"
+	s.tag_list			= "Lombok(Indonesia)"
+	s.coffeebean_list 	= "Lombok Sembal LIN"
+	s.content			= "ロンボク島はマタラム地区にある自家焙煎カフェ。インドネシアのコーヒー豆を中心に取り扱っており、ロンボク島産のコーヒー豆もあり。抽出方法もフィルター・サイフォン・フレンチぷれすなど多数から選べることができ、本格派のカフェ。店長のEviさんも気さくで良い雰囲気のカフェであり、ロンボク島でおすすめのカフェ"
+	s.description 		= "ストアが立ち並ぶ中にあり、少しわかりにくい。看板を目当てに。"
+	s.inttitle1			= ""
+	s.intcontent1		= ""	
+	s.inttitle2			= ""
+	s.intcontent2		= ""
+	s.intprofname		= ""
+	s.intprofcontent	= ""
+	s.intprofmenu		= ""
+	s.imagesprof		= "#{Rails.root}/db/image/1-1.jpg"
 	s.images            = samples
 end
 
@@ -55,7 +55,7 @@ Art.seed do |s|
 	s.intprofname		= "サンプルネーム"
 	s.intprofcontent	= "サンプルコンテンツ"
 	s.intprofmenu		= "サンプルメニュー"
-	s.imagesprof		= "#{Rails.root}/db/image/1.jpg"
+	s.imagesprof		= "#{Rails.root}/db/image/1-1.jpg"
 	s.images            = samples
 end
 
@@ -80,7 +80,7 @@ Art.seed do |s|
 	s.intprofname		= "サンプルネーム"
 	s.intprofcontent	= "サンプルコンテンツ"
 	s.intprofmenu		= "サンプルメニュー"
-	s.imagesprof		= "#{Rails.root}/db/image/1.jpg"
+	s.imagesprof		= "#{Rails.root}/db/image/1-1.jpg"
 	s.images            = samples
 end
 
@@ -106,6 +106,6 @@ Art.seed do |s|
 	s.intprofname		= "サンプルネーム"
 	s.intprofcontent	= "サンプルコンテンツ"
 	s.intprofmenu		= "サンプルメニュー"
-	s.imagesprof		= "#{Rails.root}/db/image/1.jpg"
+	s.imagesprof		= "#{Rails.root}/db/image/1-1.jpg"
 	s.images            = samples
 end
