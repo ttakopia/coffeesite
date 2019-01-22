af15ad9143bf1b7168ed7cd9c1c37b586076e31d
shohei.k <s.kihara0316@gmail.com>
2019-01-22 19:38:00 +0700

    revise the unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,7 +49,8 @@ set :log_level, :debug
 namespace :deploy do
   desc 'Restart application'
   task :restart do
-    invoke 'unicorn:restart'
+    invoke 'unicorn:stop'
+    invoke 'unicorn:start'
   end
 
   desc 'Create database'
