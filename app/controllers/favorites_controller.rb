fd47c3d3abd3ed7cef02db607af91330b99db443
shohei.k <s.kihara0316@gmail.com>
2019-01-26 19:11:34 +0700

    user login likes

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,12 +3,17 @@ class FavoritesController < ApplicationController
 
   def create
   	@favorite = Favorite.create(user_id: current_user.id, art_id: params[:art_id])
+    @favorites = Favoite.where(art_id: params[:art_id])
+    @art = Art.find(params[:art_id])
+    @arts = Art.all
   end
 
   def destroy
   	favorite = Favorite.find_by(user_id: current_user.id, art_id: params[:art_id])
     favorite.destroy
   end
+
+  private
 
   def set_art
   	@art = Art.find(params[:art_id])
