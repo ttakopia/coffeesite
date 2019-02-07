1faaf844544e13186c388c8b4771a1e633cf2fe6
shohei.k <s.kihara0316@gmail.com>
2019-02-07 09:10:20 +0700

    revise the fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
   if Rails.env.development? || Rails.env.test?
   config.storage = :file 
   else
-  config.storage :fog
+  config.storage = :fog
   config.fog_provider = 'fog/aws' # 追加
   config.fog_credentials = {
     provider: 'AWS',
