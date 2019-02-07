1844721496b4aa4a10ea4beb6e299570b1adda16
shohei.k <s.kihara0316@gmail.com>
2019-02-07 09:31:44 +0700

    r

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,10 +4,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production? || Rails.env.staging?
-    storage :fog
-  else
+  if Rails.env.development? || Rails.env.test?
     storage :file
+  else
+    storage :fog
   end
   # storage :fog
 
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,19 +3,16 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test?
-  config.storage = :file 
-  else
-  config.storage = :fog
   config.fog_provider = 'fog/aws' # 追加
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-    region: 'ap-southeast-1' #シンガポール
+    region: 'ap-southeast-1', #シンガポール
+    endpoint: 'https://s3.amazonaws.com'
   }
 
   config.fog_directory  = 'bucket-name-coffeeseeker'
-  config.asset_host = 'https://s3.amazonaws.com/bucket-name-coffeeseeker'
-  end
+  config.fog_public = false 
+  config.storage = :fog
 end
